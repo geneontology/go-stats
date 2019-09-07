97953fd3af09626fdbee718fa57a3855d36b3450
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2019-09-07 02:40:34 -0700

    Pypi package updated

--- libraries/go-stats/setup.py
+++ libraries/go-stats/setup.py
@@ -5,16 +5,17 @@ with open("README.md", "r") as fh:
 
 setup(
     name='go-stats',
-    version='1.1.2',
+    version='1.1.3',
     description='Python Library to generate statistics on a Gene Ontology (GO) release',
     py_modules=["go_stats"],
     packages=[''],
     url="https://github.com/geneontology/go-stats",
     author="Laurent-Philippe Albou",
     author_email="laurent.albou@lbl.gov",
-    keywords=["GO", "stats", "GOLR", "statistics", "Gene Ontology"],
+    keywords=["GO", "Gene Ontology", "annotation", "ontology", "stats", "changes", "GOLR", "statistics"],
     install_requires=[
         'requests',
+        'networkx'
     ],
 
     classifiers=[
