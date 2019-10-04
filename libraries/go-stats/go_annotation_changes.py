41583e0245f01c584b0eec496ad8a43969a2c2f3
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2019-10-04 10:15:58 -0700

    .

--- libraries/go-stats/go_annotation_changes.py
+++ libraries/go-stats/go_annotation_changes.py
@@ -73,8 +73,7 @@ def create_text_report(json_changes):
 
     text_report = "CHANGES IN GO ANNOTATIONS"
 
-    text_report += "\n\nSUMMARY: CURRENT_RELEASE (" + json_changes["releases_compared"]["current"] + ")"
-    text_report += "\nrelease_date\t" + json_changes["summary"]["current"]["release_date"]
+    text_report += "\n\nSUMMARY: CURRENT_RELEASE (" + json_changes["summary"]["current"]["release_date"] + ")"
     text_report += "\nannotated bioentities:\t" + str(json_changes["summary"]["current"]["bioentities"])
     text_report += "\nannotated taxa:\t" + str(json_changes["summary"]["current"]["taxa"]) + "\t" + str(json_changes["summary"]["current"]["taxa_filtered"]) + " (with more than 1000 annotations)"
     text_report += "\nannotations:\t" + str(json_changes["summary"]["current"]["annotations"]["total"])
@@ -83,8 +82,7 @@ def create_text_report(json_changes):
     for key, val in json_changes["summary"]["current"]["annotations"]["by_evidence_cluster"].items():
         text_report += "\nannotations by evidence cluster " + key + ":\t" + str(val)
 
-    text_report += "\n\nSUMMARY: PREVIOUS (" + json_changes["releases_compared"]["previous"] + ")"
-    text_report += "\nrelease_date\t" + json_changes["summary"]["previous"]["release_date"]
+    text_report += "\n\nSUMMARY: PREVIOUS (" + json_changes["summary"]["previous"]["release_date"] + ")"
     text_report += "\nannotated bioentities:\t" + str(json_changes["summary"]["previous"]["bioentities"])
     text_report += "\nannotated taxa:\t" + str(json_changes["summary"]["previous"]["taxa"]) + "\t" + str(json_changes["summary"]["previous"]["taxa_filtered"]) + " (with more than 1000 annotations)"
     text_report += "\nannotations:\t" + str(json_changes["summary"]["previous"]["annotations"]["total"])
