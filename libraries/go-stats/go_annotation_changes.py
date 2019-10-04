0395e19e6027ad1b3cc2cc704ae32384d1ecab12
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2019-10-04 10:07:00 -0700

    tsv output

--- libraries/go-stats/go_annotation_changes.py
+++ libraries/go-stats/go_annotation_changes.py
@@ -174,12 +174,12 @@ def create_text_report(json_changes):
 
     
     text_report += "\n\n" + str(len(json_changes["detailed_changes"]["taxa"]["added"])) + " ADDED TAXA"
-    for key, val in json_changes["detailed_changes"]["taxa"]["added"].items():
-        text_report += "\n" + key + "\t" + str(val)
+    for taxon in json_changes["detailed_changes"]["taxa"]["added"].items():
+        text_report += "\n" + taxon
     
     text_report += "\n\n" + str(len(json_changes["detailed_changes"]["taxa"]["removed"])) + " REMOVED TAXA"
-    for key, val in json_changes["detailed_changes"]["taxa"]["removed"].items():
-        text_report += "\n" + key + "\t" + str(val)
+    for taxon in json_changes["detailed_changes"]["taxa"]["removed"]:
+        text_report += "\n" + taxon
 
 
     text_report += "\n\nCHANGES IN REFERENCES AND PMIDS\n"
