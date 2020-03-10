4aebb1fbc33bfea2180316c6506710afa68741a0
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2020-03-09 21:41:38 -0700

    More modular, new stats

--- libraries/go-stats/go_reports.py
+++ libraries/go-stats/go_reports.py
@@ -6,137 +6,7 @@ import go_stats
 import go_ontology_changes
 import go_annotation_changes
 
-
-def merge_dict(dict_total, dict_diff):
-    new_dict = { }
-    for key, val in dict_total.items():
-        if type(val) == str:
-            new_dict[key] = val
-        elif type(val) == int or type(val) == float:
-            if val == 0:
-                diff_val = dict_diff[key] if key in dict_diff else 0
-                new_dict[key] = str(diff_val) + " / " + str(val) + " (0%) "
-            else:
-                diff_val = dict_diff[key] if key in dict_diff else 0
-                new_dict[key] = str(diff_val) + " / " + str(val) + " (" + str(round(100 * diff_val / val, 2)) + "%) "
-        elif type(val) == dict:
-            diff_val = dict_diff[key] if key in dict_diff else { }
-            new_dict[key] = merge_dict(val, diff_val)
-        else:
-            print("should not happened ! " , val , type(val))
-    return new_dict
-
-def minus_dict(dict1, dict2):
-    new_dict = { }
-    for key, val in dict1.items():
-        if type(val) == str:
-            new_dict[key] = val
-        elif type(val) == int or type(val) == float:
-                diff_val = dict2[key] if key in dict2 else 0
-                new_dict[key] = val - diff_val
-        elif type(val) == dict:
-            diff_val = dict2[key] if key in dict2 else { }
-            new_dict[key] = merge_dict(val, diff_val)
-        else:
-            print("should not happened ! " , val , type(val))
-    return new_dict    
-
-
-def has_taxon(stats, taxon_id):
-    for taxon in stats["annotations"]["by_taxon"]:
-        if taxon_id in taxon:
-            return True
-    return False
-
-def added_removed_species(current_stats, previous_stats):
-    results = {
-        "added" : { },
-        "removed" : { }
-    }
-
-    for taxon in current_stats["annotations"]["by_taxon"]:
-        taxon_id = taxon.split("|")[0]
-        if not has_taxon(previous_stats, taxon_id):
-            results["added"][taxon] = current_stats["annotations"]["by_taxon"][taxon]
-
-    for taxon in previous_stats["annotations"]["by_taxon"]:
-        taxon_id = taxon.split("|")[0]
-        if not has_taxon(current_stats, taxon_id):
-            results["removed"][taxon] = previous_stats["annotations"]["by_taxon"][taxon]
-        
-    return results
-
-
-def alter_annotation_changes(current_stats, previous_stats, json_annot_changes):
-    addrem_species = added_removed_species(current_stats, previous_stats)
-    # print("INITIAL: ", json_annot_changes)
-    # print("DEBUG: ", addrem_species)
-
-    altered_json_annot_changes = {
-        # "releases_compared" : {
-        #     "current" : current_stats["release_date"],
-        #     "previous" : previous_stats["release_date"]
-        # },
-        "summary" : {
-            "current" : {
-                "release_date" : current_stats["release_date"],
-                "annotations" : {
-                    "total" : current_stats["annotations"]["total"],
-                    "by_aspect" : current_stats["annotations"]["by_aspect"],
-                    "by_evidence_cluster" : current_stats["annotations"]["by_evidence"]["cluster"],
-                },
-                "bioentities" : current_stats["bioentities"]["total"],
-                "taxa" : current_stats["taxa"]["total"],
-                "taxa_filtered" : current_stats["taxa"]["filtered"],
-                "references" : current_stats["references"]["all"]["total"],
-                "pmids" : current_stats["references"]["pmids"]["total"]
-            },
-            "previous" : {
-                "release_date" : previous_stats["release_date"],
-                "annotations" : {
-                    "total" : previous_stats["annotations"]["total"],
-                    "by_aspect" : previous_stats["annotations"]["by_aspect"],
-                    "by_evidence_cluster" : previous_stats["annotations"]["by_evidence"]["cluster"],
-                },
-                "bioentities" : previous_stats["bioentities"]["total"],
-                "taxa" : previous_stats["taxa"]["total"],
-                "taxa_filtered" : previous_stats["taxa"]["filtered"],
-                "references" : previous_stats["references"]["all"]["total"],
-                "pmids" : previous_stats["references"]["pmids"]["total"]
-            },
-            "changes" : {
-                "annotations" : {
-                    "total" : current_stats["annotations"]["total"] - previous_stats["annotations"]["total"],
-                    "by_aspect" : minus_dict(current_stats["annotations"]["by_aspect"], previous_stats["annotations"]["by_aspect"]),
-                    "by_evidence_cluster" : minus_dict(current_stats["annotations"]["by_evidence"]["cluster"], previous_stats["annotations"]["by_evidence"]["cluster"]),
-                },
-                "bioentities" : current_stats["bioentities"]["total"] - previous_stats["bioentities"]["total"],
-                "taxa" : {
-                    "total" : current_stats["taxa"]["total"] - previous_stats["taxa"]["total"],
-                    "filtered" : current_stats["taxa"]["filtered"] - previous_stats["taxa"]["filtered"],
-                    "added" : len(addrem_species["added"]),
-                    "removed" : len(addrem_species["removed"])
-                },
-                "references" : {
-                    "total" : current_stats["references"]["all"]["total"] - previous_stats["references"]["all"]["total"],
-                    "added" : 0,
-                    "removed" : 0
-                },
-                "pmids" : {
-                    "total" : current_stats["references"]["pmids"]["total"] - previous_stats["references"]["pmids"]["total"],
-                    "added" : 0,
-                    "removed" : 0
-                }
-            },
-        },
-        "detailed_changes" : {
-            "annotations" : json_annot_changes["annotations"],
-            "taxa" : addrem_species,
-            "bioentities" : json_annot_changes["bioentities"],
-            "references" : json_annot_changes["references"]
-        }
-    }
-    return altered_json_annot_changes  
+import go_stats_utils as utils
 
 
 def print_help():
@@ -209,10 +79,10 @@ def main(argv):
     # 2 - Executing go_ontology_changes script
     print("\n\n2 - EXECUTING GO_ONTOLOGY_CHANGES SCRIPT...\n")
     json_onto_changes = go_ontology_changes.compute_changes(current_obo_url, previous_obo_url)
-    go_annotation_changes.write_json(output_rep + "go-ontology-changes.json", json_onto_changes)
+    utils.write_json(output_rep + "go-ontology-changes.json", json_onto_changes)
 
     tsv_onto_changes = go_ontology_changes.create_text_report(json_onto_changes) 
-    go_annotation_changes.write_text(output_rep + "go-ontology-changes.tsv", tsv_onto_changes)
+    utils.write_text(output_rep + "go-ontology-changes.tsv", tsv_onto_changes)
 
     # with open('newstats/2019-06/go-ontology-changes.json', 'r') as myfile:
     #     data=myfile.read()
@@ -233,7 +103,7 @@ def main(argv):
 
     # 4 - Refining go-stats with ontology stats
     print("\n\n4 - EXECUTING GO_REFINE_STATS SCRIPT...\n")
-    merged_annotations_diff = merge_dict(json_stats, json_annot_changes)
+    merged_annotations_diff = utils.merge_dict(json_stats, json_annot_changes)
     json_annot_changes = merged_annotations_diff
 
 
@@ -244,6 +114,10 @@ def main(argv):
     ontology["changes_obsolete_terms"] = json_onto_changes["summary"]["changes"]["obsolete_terms"]
     ontology["changes_merged_terms"] = json_onto_changes["summary"]["changes"]["merged_terms"]
 
+    ontology["changes_biological_process_terms"] = json_onto_changes["summary"]["changes"]["biological_process_terms"]
+    ontology["changes_molecular_function_terms"] = json_onto_changes["summary"]["changes"]["molecular_function_terms"]
+    ontology["changes_cellular_component_terms"] = json_onto_changes["summary"]["changes"]["cellular_component_terms"]
+
     json_stats = {
         "release_date" : json_stats["release_date"],
         "ontology" : ontology,
@@ -252,7 +126,7 @@ def main(argv):
         "bioentities" : json_stats["bioentities"],
         "references" : json_stats["references"]
     }
-    go_stats.write_json(output_rep + "go-stats.json", json_stats)
+    utils.write_json(output_rep + "go-stats.json", json_stats)
 
 
     json_stats_no_pb = {
@@ -263,7 +137,7 @@ def main(argv):
         "bioentities" : json_stats_no_pb["bioentities"],
         "references" : json_stats_no_pb["references"]
     }
-    go_stats.write_json(output_rep + "go-stats-no-pb.json", json_stats_no_pb)
+    utils.write_json(output_rep + "go-stats-no-pb.json", json_stats_no_pb)
 
 
     annotations_by_reference_genome = json_stats["annotations"]["by_model_organism"]
@@ -298,6 +172,7 @@ def main(argv):
         "annotations" : {
             "total" : json_stats["annotations"]["total"],
             "total_no_pb" : json_stats_no_pb["annotations"]["total"],
+            "total_pb" : json_stats["annotations"]["total"] - json_stats_no_pb["annotations"]["total"],
             "by_aspect" : {
                 "P" : json_stats["annotations"]["by_aspect"]["P"],
                 "F" : json_stats["annotations"]["by_aspect"]["F"],
@@ -338,19 +213,19 @@ def main(argv):
     # removing by_reference_genome.by_evidence
     for gen in json_stats_summary["annotations"]["by_model_organism"]:
         del json_stats_summary["annotations"]["by_model_organism"][gen]["by_evidence"]
-    go_stats.write_json(output_rep + "go-stats-summary.json", json_stats_summary)
+    utils.write_json(output_rep + "go-stats-summary.json", json_stats_summary)
 
 
     # This is to modify the structure of the annotation changes based on recent requests
-    json_annot_changes = alter_annotation_changes(json_stats, previous_stats, json_annot_changes)
-    go_annotation_changes.write_json(output_rep + "go-annotation-changes.json", json_annot_changes)
+    json_annot_changes = go_annotation_changes.alter_annotation_changes(json_stats, previous_stats, json_annot_changes)
+    utils.write_json(output_rep + "go-annotation-changes.json", json_annot_changes)
     tsv_annot_changes = go_annotation_changes.create_text_report(json_annot_changes)
-    go_annotation_changes.write_text(output_rep + "go-annotation-changes.tsv", tsv_annot_changes)
+    utils.write_text(output_rep + "go-annotation-changes.tsv", tsv_annot_changes)
 
-    json_annot_no_pb_changes = alter_annotation_changes(json_stats_no_pb, previous_stats_no_pb, json_annot_no_pb_changes)
-    go_annotation_changes.write_json(output_rep + "go-annotation-changes_no_pb.json", json_annot_no_pb_changes)
+    json_annot_no_pb_changes = go_annotation_changes.alter_annotation_changes(json_stats_no_pb, previous_stats_no_pb, json_annot_no_pb_changes)
+    utils.write_json(output_rep + "go-annotation-changes_no_pb.json", json_annot_no_pb_changes)
     tsv_annot_changes_no_pb = go_annotation_changes.create_text_report(json_annot_no_pb_changes)
-    go_annotation_changes.write_text(output_rep + "go-annotation-changes_no_pb.tsv", tsv_annot_changes_no_pb)
+    utils.write_text(output_rep + "go-annotation-changes_no_pb.tsv", tsv_annot_changes_no_pb)
 
 
     print("SUCCESS.")
